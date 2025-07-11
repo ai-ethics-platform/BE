c638c61eb2d7eb8752ea834e68e9015bbf91515e
Hwang-Jaeryeong <jryeong67@naver.com>
2025-07-11 21:11:52 +0900

    add: cors

--- alembic/env.py
+++ alembic/env.py
@@ -5,6 +5,8 @@ from sqlalchemy import pool
 
 from alembic import context
 
+from app.db.base import Base  # Base는 declarative_base()로 생성된 객체여야 함
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -18,7 +20,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -65,7 +67,9 @@ def run_migrations_online() -> None:
 
     with connectable.connect() as connection:
         context.configure(
-            connection=connection, target_metadata=target_metadata
+            connection=connection,
+            target_metadata=target_metadata,
+            # ... 기타 옵션 ...
         )
 
         with context.begin_transaction():
